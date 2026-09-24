4efb665fc1975ce9822d39a021292fe1e8e4589e
RAGHAV GABA <raghavgaba0910@gmail.com>
2026-09-24 23:34:22 +0530

    Time: 4 ms (72.18%) | Memory: 14.6 MB (86.61%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -1,13 +1,18 @@
-
 class Solution {
 public:
     int firstUniqChar(string s) {
-       unordered_map<char, int> mp;
+        int hash[26] = {0};
 
-        for (auto a : s) mp[a]++;
-        for (int i = 0; i < s.size(); i++)
-            if(mp[s[i]] == 1)return i;
+        for (int i = 0; i < s.size(); i++) {
+            hash[s[i] - 'a']++;
+        }
+
+        for (int i = 0; i < s.size(); i++) {
+            if (hash[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
 
         return -1;
     }
-}; 
+};
